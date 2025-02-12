48df24a244e739b8a1c93973d1efa4a541410580
chimkem <126956034+chimkem@users.noreply.github.com>
2025-02-12 11:28:39 +0200

    o7

--- WS02a/app.js
+++ WS02a/app.js
@@ -1,6 +1,8 @@
 const message = 'Hello, Node.js!'
 console.log(message);
 
+
+// Math module
 const maths=require('./modules/math');
 
 console.log("\nInside maths variable : ");
@@ -10,3 +12,7 @@ console.log("Subtraction is " +maths.subtract(7,7));
 console.log("Multiplication is " +maths.multiply(7,7));
 console.log("Division is " +maths.divide(7,7));
 
+// String Utils module
+const strUtils=require('./modules/stringUtils');
+console.log("\nInside stringUtils variable : ");
+console.log(strUtils);
