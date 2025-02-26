30b6e9331091d40ade4427806d49a404fa0476de
I'm Just Ken <126956034+chimkem@users.noreply.github.com>
2025-02-27 01:00:49 +0200

    Yhtä tehtävää vaille valmis

--- WS02b/app.js
+++ WS02b/app.js
@@ -1,4 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+const message = '--- My Second node.js app ---'
+// --------------------------------------------
 // Message
-const message = 'Hello, this is my second app.js!'
-const smessage = 'Hello nodemon!'
-console.log(message, smessage);
+console.log(message);
+
+// Read file
+fs.readFile(path.join(__dirname, 'files', 'example.txt'), 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+})
+// Write file
+fs.writeFile(path.join(__dirname, 'files', 'output.txt'), 'Tässä tekstiä, joka ei tarkoita mitään...', (err) => {
+    if (err) throw err;
+
+    // Modify file
+    fs.appendFile(path.join(__dirname, 'files', 'output.txt'), '\n\nKappas, olenkin editoitu tiedosto! :D', (err) => {
+        if (err) throw err;
+    })
+    console.log('Tiedosto luotu onnistuneesti!');
+})
+// Delete files
+fs.unlink(path.join(__dirname, 'files', 'temp.txt'), (err) => {
+    if (err) throw err;
+    console.log('Tiedosto poistettu onnistuneesti!');
+})
+// Create a directory if it does not exist
+if (!fs.existsSync('./testDir')) {
+    fs.mkdir('./testDir', (err) => {
+        if (err) throw err;
+        console.log('Kansio luotu onnistuneesti!');
+    });
+}
+// Remove a directory if it does exist
+if (fs.existsSync('./testDir')) {
+    fs.rmdir('./testDir', (err) => {
+        if (err) throw err;
+        console.log('Kansio poistettu onnistuneesti!');
+    });
+}
+// Exit on uncaught errors
+process.on('uncaughtException', err => {
+    console.error(`Johtain meni vikaan! ${err}`);
+    process.exit(1);
+})
--- WS02b/readme.md
+++ WS02b/readme.md
@@ -10,8 +10,8 @@ Npm versio: 11.1.0
 Exercise 1: ✅ Initializing a Node.js Project and Installing Packages  
 Exercise 2: ✅ Managing Development Dependencies   
 Exercise 3: ✅ Updating and Removing Packages  
-Exercise 4: Reading Files  
-Exercise 5: Writing Files  
-Exercise 6: Deleting Files  
+Exercise 4: ✅ Reading Files  
+Exercise 5: ✅ Writing Files  
+Exercise 6: ✅ Deleting Files  
 Exercise 7: Working with Directories  
 Exercise 8: Watching for File Changes  
