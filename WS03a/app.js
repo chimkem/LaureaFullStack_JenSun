b95b825e2f05a4cb3dda6f4a7d964956a3c227d6
I'm Just Ken <126956034+chimkem@users.noreply.github.com>
2025-03-11 15:29:55 +0200

    Set up Express to serve static files from the public directory using express.static

--- WS03a/app.js
+++ WS03a/app.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/about', function (req, res) {
+app.use('/index.html', express.static(path.join(__dirname, 'public', 'index.html')));
+
+app.get('/about', (req, res) => {
     res.send('About Page');
 });
 
-app.get('/contact', function (req, res) {
+app.get('/contact', (req, res) => {
     res.send('Contact Page');
 });
 
-app.get('/services', function (req, res) {
+app.get('/services', (req, res) => {
     res.send('Services Page');
 });
 
 // 404 route
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
     res.send('Error, cannot find the page', 404)
 });
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log('App listening to port: ' + PORT)
 });
